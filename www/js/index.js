40f0fff97b2eafe424efcdd84b052925c01c62ef
Jonathan Tweedy <jetweedy@users.noreply.github.com>
2019-11-16 14:11:56 -0500

    Add files via upload

--- www/js/index.js
+++ www/js/index.js
@@ -1,45 +1,41 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+
+
+function drawPatient() {
+	
+}
  
-function runTest() {
+var patients = [];
+function drawPatients(x) {
 	try {
-		$.get("https://pyfhir.herokuapp.com/getdata", function(x) {
-			try {
-				var output = "";
-				$("#fhirapp").html("");
-				var table = $("<table>");
-				table.append($("<tr><th>First</th><th>Last</th><th>Phone</th></tr>"));
-				for (var p in x) {
-					table.append($("<tr><td>"
-						+x[p].firstname+"</td><td>"
-						+x[p].lastname+"</td><td>"
-						+x[p].phone+"</td></tr>"));
-				}
-				$("#fhirapp").append(table);
-			} catch(er) {
-				alert("issue with html");
-			}
+		var output = "";
+		$("#fhirapp").html("");
+		var table = $("<table>");
+		table.append($("<tr><th>First</th><th>Last</th><th>Phone</th></tr>"));
+		for (var p in x) {
+			table.append($("<tr><td>"
+				+x[p].firstname+"</td><td>"
+				+x[p].lastname+"</td><td>"
+				+x[p].phone+"</td></tr>"));
+		}
+		$("#fhirapp").append(table);
+	} catch(er) {
+		alert("issue with html");
+	}	
+}
+function runSearchName(sn) {
+	$("#fhirapp").html("");
+	var img = $("<img>").attr("src", "img/loading.gif");
+	$("#fhirapp").append( img );
+	try {
+		$.get("https://pyfhir.herokuapp.com/getdata/"+sn, function(x) {
+			patients = x;
+			drawPatients(x);
 		});
 	} catch(er) {
 		alert("issue with get");
 	}
 }
+
 
 
 
@@ -59,7 +55,9 @@ var app = {
     receivedEvent: function(id) {
 		switch(id) {
 			case "ready":
-				runTest();
+				$("#searchbutton").on("click", function() {
+					runSearchName( $("#searchname").val() );
+				});
 				break;
 			default:
 				break;
